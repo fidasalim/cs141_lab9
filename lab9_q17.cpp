98e4b863fc5e3c8c52841c861f25e43251c67c51
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-10-21 02:35:33 +0530

    Update lab9_q17.cpp

--- lab9_q17.cpp
+++ lab9_q17.cpp
@@ -8,13 +8,81 @@ void strcpy(char *p, char *q){
 		p++; 
 		q++;
 		}
+}//including library
+#include<iostream>
+using namespace std;
+void strcpy(char *p, char *q){
+	//copying the array p to array r
+	while (*p){
+		*q = *p;
+		p++; 
+		q++;
+		}
+	*q = '\0';	
+}
+void strcat(char *a, char *b){
+	//copying the array p to array r
+	for (int i = 0;i<50;i++){
+	    if(i<12){
+	        *a=*a;
+	    } 
+	    else{
+	        *(a+i)=*b;
+	        b++;
+	    }
+	}
+	
+}
+void strlen(char *p){
+    char m;
+    //defining the length of a string
+    for(int i=0;i<50;i++){
+        m =*(p+i);
+        if(m=='\0'){
+            cout<<"THe length of the string p is "<<i<<endl;
+            break;
+        }
+    }
+    
 }
 
 int main(){
 	char p[13] = "Good Morning";
 	char q[13];
+	char a[50] = "Good Morning";
+	char b[50] = " to everybody";
 	//calling for function
 	strcpy(p,q);
 	cout << q<< endl;
+	strcat(a,b);
+	cout <<a<< endl;
+	return 0;
+}
+
+void strcat(char *p, char *r){
+	//copying the array p to array r
+	for (int i = 0;i<50;i++){
+	    if(i<12){
+	        *p=*p;
+	    } 
+	    else{
+	        *(p+i)=*r;
+	        r++;
+	    }
+	}
+	
+}
+
+int main(){
+	char p[13] = "Good Morning";
+	char q[13];
+	char r[50] = " to everybody";
+	//calling for function
+	strcpy(p,q);
+	cout << q<< endl;
+	strcat(p,q);
+	cout << p<< endl;
+	cout<<"The string 'p' is "<<p<<endl;
+	strlen(p);
 	return 0;
 }
